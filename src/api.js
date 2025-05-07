3c54084ed901fad38b84828a854ebb3485ccf7e1
Douglas Seramim <douglasfseramim@gmail.com>
2025-05-07 20:12:02 -0300

    update packages

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api-nota-facil.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchNotes = async () => {
     try {
