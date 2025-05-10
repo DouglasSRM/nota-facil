f39a162bf222f54f81a15cc6829e8cb8c4863886
Douglas Seramim <douglasfseramim@gmail.com>
2025-05-10 00:11:03 -0300

    update api

--- src/api.js
+++ src/api.js
@@ -1,39 +1,39 @@
-import axios from "axios";
+import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export const fetchNotes = async () => {
     try {
-        const response = await axios.get(`${API_URL}/notes`);
-        return response.data.data
+        const response = await axios.get(`${API_URL}/get`)
+        return response.data
     } catch (error) {
-        console.error("Erro ao buscar notas", error);
-        return [];
+        console.error("Erro ao buscar notas", error)
+        return []
     }
-};
+}
 
 export const createNote = async (note) => {
     try {
-        const response = await axios.post(`${API_URL}/create_note`, note);
-        return response.data.data;
+        const response = await axios.post(`${API_URL}/post`, note)
+        return response.data
     } catch (error) {
-        console.error("Erro ao criar nota", error);
+        console.error("Erro ao criar nota", error)
     }
 };
 
 export const updateNote = async (note) => {
     try {
-        const response = await axios.put(`${API_URL}/update_note/${note.id}`, note);
-        return response.data.data;
+        const response = await axios.put(`${API_URL}/update/${note.id}`, note)
+        return response.data
     } catch (error) {
-        console.error("Erro ao atualizar nota", error);
+        console.error("Erro ao atualizar nota", error)
     }
-};
+}
 
 export const deleteNotes = async (noteIds) => {
     try {
-        await axios.delete(`${API_URL}/delete_notes/`, { data: { ids: noteIds } });
+        await axios.post(`${API_URL}/delete`, { ids: noteIds } )
     } catch (error) {
-        console.error("Erro ao deletar notas", error);
+        console.error("Erro ao deletar notas", error)
     }
-};
+}
